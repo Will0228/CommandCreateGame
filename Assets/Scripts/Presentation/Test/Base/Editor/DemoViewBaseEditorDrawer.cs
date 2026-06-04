85cb583c0afac15b377c2c1d42483d4015688e13
DAIKI\kis11 <kis1181100133@gmail.com>
2026-06-05 01:11:34 +0900

    animationControllerをデモで操作できるように

--- Assets/Scripts/Presentation/Test/Base/Editor/DemoViewBaseEditorDrawer.cs
+++ Assets/Scripts/Presentation/Test/Base/Editor/DemoViewBaseEditorDrawer.cs
@@ -6,13 +6,16 @@ using UnityEngine;
 
 namespace Presentation.DemoViewTest
 {
-   internal sealed class DemoViewBaseEditorDrawer
+    internal sealed class DemoViewBaseEditorDrawer
     {
         private readonly Subject<Unit> _onExecuteMethodSubject = new();
         public Observable<Unit> OnExecuteMethodAsObservable => _onExecuteMethodSubject.AsObservable();
-        
+
+        private readonly Subject<AnimationClip> _onExecuteAnimationSubject = new();
+        public Observable<AnimationClip> OnExecuteAnimationAsObservable => _onExecuteAnimationSubject.AsObservable();
+
         private SerializedObject _serializedObject;
-        
+
         public void Configure(SerializedObject serializedObject)
         {
             _serializedObject = serializedObject;
@@ -25,49 +28,53 @@ namespace Presentation.DemoViewTest
             EditorGUILayout.BeginVertical(EditorStyles.helpBox);
             {
                 EditorGUILayout.LabelField("Demo SetUp", EditorStyles.boldLabel);
-                    
+
                 _serializedObject.Update();
-                
+
                 foreach (var parameter in commonParameters)
                 {
                     CreateField(parameter);
                 }
-                
+
                 EditorGUILayout.Space();
 
                 if (GUILayout.Button("Invoke"))
                 {
                     _onExecuteMethodSubject.OnNext(Unit.Default);
                 }
-                
+
                 _serializedObject.ApplyModifiedProperties();
             }
             EditorGUILayout.EndVertical();
-            
+
             EditorGUILayout.Space();
             EditorGUILayout.BeginVertical(EditorStyles.helpBox);
             {
                 EditorGUILayout.LabelField("Animator SetUp", EditorStyles.boldLabel);
-                    
+
                 _serializedObject.Update();
-                
+
                 foreach (var parameter in animatorParameters)
                 {
-                    CreateField(parameter);
+                    var animationClips = CreateAnimatorControllerField(parameter);
+                    if (animationClips != null)
+                    {
+                        foreach (var clip in animationClips)
+                        {
+                            EditorGUILayout.Space();
+                            if (GUILayout.Button($"{clip.name} Invoke"))
+                            {
+                                _onExecuteAnimationSubject.OnNext(clip);
+                            }
+                        }
+                    }
                 }
-                
-                EditorGUILayout.Space();
 
-                if (GUILayout.Button("Invoke"))
-                {
-                    _onExecuteMethodSubject.OnNext(Unit.Default);
-                }
-                
                 _serializedObject.ApplyModifiedProperties();
             }
             EditorGUILayout.EndVertical();
         }
-        
+
         private void CreateField(DemoViewBaseEditorParameterDefinition.IGuiParameter parameter)
         {
             if (parameter is DemoViewBaseEditorParameterDefinition.IntParameter intParameter)
@@ -78,7 +85,7 @@ namespace Presentation.DemoViewTest
                     parameter.SetField(newValue.ToString());
                 }
             }
-            else if(parameter is DemoViewBaseEditorParameterDefinition.SpriteParameter spriteParameter)
+            else if (parameter is DemoViewBaseEditorParameterDefinition.SpriteParameter spriteParameter)
             {
                 var newValue = (Sprite)EditorGUILayout.ObjectField(parameter.ParameterName, spriteParameter.Value, typeof(Sprite), false);
                 if (newValue != spriteParameter.Value)
@@ -86,7 +93,7 @@ namespace Presentation.DemoViewTest
                     parameter.SetField(AssetDatabase.GetAssetPath(newValue));
                 }
             }
-            else if(parameter is DemoViewBaseEditorParameterDefinition.AudioParameter audioParameter)
+            else if (parameter is DemoViewBaseEditorParameterDefinition.AudioParameter audioParameter)
             {
                 var newValue = (AudioClip)EditorGUILayout.ObjectField(parameter.ParameterName, audioParameter.Value, typeof(AudioClip), false);
                 if (newValue != audioParameter.Value)
@@ -94,14 +101,32 @@ namespace Presentation.DemoViewTest
                     parameter.SetField(AssetDatabase.GetAssetPath(newValue));
                 }
             }
-            else if (parameter is DemoViewBaseEditorParameterDefinition.AnimatorParameter animatorControllerParameter)
+        }
+
+        private IReadOnlyList<AnimationClip> CreateAnimatorControllerField(DemoViewBaseEditorParameterDefinition.IGuiParameter parameter)
+        {
+            if (parameter is DemoViewBaseEditorParameterDefinition.AnimatorParameter animatorControllerParameter)
             {
-                var newValue = (AnimatorController)EditorGUILayout.ObjectField(parameter.ParameterName, animatorControllerParameter.Value, typeof(AnimatorController), false);
-                if (newValue != animatorControllerParameter.Value)
+                var newAnimationControllerValue = (AnimatorController)EditorGUILayout.ObjectField(parameter.ParameterName, animatorControllerParameter.AnimatorControllerValue, typeof(AnimatorController), false);
+                if (newAnimationControllerValue != animatorControllerParameter.AnimatorControllerValue)
                 {
-                    parameter.SetField(AssetDatabase.GetAssetPath(newValue));
+                    parameter.SetField($"{DemoViewBaseEditorParameterDefinition.AnimatorParameter.ANIMATOR_CONTROLLER_TAG}\\{AssetDatabase.GetAssetPath(newAnimationControllerValue)}");
                 }
+
+                var animationClipValues = animatorControllerParameter.AnimationClipValues;
+                for (int i = 0; i < animationClipValues.Count; i++)
+                {
+                    var newAnimationClipValue = (AnimationClip)EditorGUILayout.ObjectField(animationClipValues[i].name, animationClipValues[i], typeof(AnimationClip), false);
+                    if (newAnimationClipValue != animationClipValues[i])
+                    {
+                        parameter.SetField($"{DemoViewBaseEditorParameterDefinition.AnimatorParameter.ANIMATION_CLIP_TAG}\\{newAnimationClipValue.name}\\{i}");
+                    }
+                }
+
+                return animationClipValues;
             }
+
+            return null;
         }
     }
 }
